798d1c8a17b2946fe5a342610a7f853f0fd6811f
marinellaom <moral1820393@mkt.ceu.edu.ph>
2019-01-08 13:21:00 +0800

    add button

--- BasicElements.java
+++ BasicElements.java
@@ -4,6 +4,13 @@ public class BasicElements{
 
 	public static void main (String args[]){
 		JFrame myFrame = new JFrame();
+		JButton myButton = new JButton();
+
+		myButton.setSize (200, 50);
+
+		myFrame.setLayout(null);
+		
+		myFrame.add(myButton);
 
 		myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
 		myFrame.setVisible(true);
